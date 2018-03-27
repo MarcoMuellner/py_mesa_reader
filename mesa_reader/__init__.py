290f68253399f14aa5f8670ae447bd1087ca279e
mm7490 <muellnermarco@gmail.com>
2018-03-27 10:32:40 +0200

    Appended init.py so that it reads all classes accordingly

--- mesa_reader/__init__.py
+++ mesa_reader/__init__.py
@@ -4,6 +4,7 @@ import re
 
 import numpy as np
 
-
-
+from MesaHandler.py_mesa_reader.mesa_reader.MesaData import *
+from MesaHandler.py_mesa_reader.mesa_reader.MesaLogDir import *
+from MesaHandler.py_mesa_reader.mesa_reader.MesaProfileIndex import *
 
